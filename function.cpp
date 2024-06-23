47a9b4d980cd10a30da5189b1d3e913c52cea20b
sanaretty <sanikayovi@gmail.com>
2024-06-23 10:53:57 +0530

    First commit

---
+++ function.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printName(string name){
+    cout<<"hey"<< name;
+}
+int main(){
+    string name;
+    cin>>name;
+    printName(name);
+    return 0;
+}
